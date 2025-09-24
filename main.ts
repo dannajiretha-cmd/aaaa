4c904000ad4f0f34541bfe87d2b8ff01fed301e9
dannajiretha-cmd <dannajiretha@gmail.com>
2025-09-23 19:53:26 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
+})
+input.onButtonPressed(Button.AB, function () {
+    SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
+})
